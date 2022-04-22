1d9e1c1e1fc933a20e6b0e42e351f40480684dad
3wRI2uzLrLkm <91275667+3wRI2uzLrLkm@users.noreply.github.com>
2022-04-23 00:55:35 +0800

    Add files via upload

---
+++ tr.py
@@ -0,0 +1,21 @@
+login = "noob"
+password = "binance"
+
+name = input("login: ")
+
+if name != login: print("user not found"), exit()
+
+pass_user = input("password: ")
+
+if pass_user == "binance":
+	print("\nok bro\nbalance: 0💩")
+
+else: print("cum"), exit()
+
+trade = input("trade?\n\n")
+
+print()
+
+if trade == "yes": print("balance: 10000💸")
+else: print("balance: -100👉👈")
+	
